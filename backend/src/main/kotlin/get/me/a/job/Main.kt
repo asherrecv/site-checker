0e8c6d282ea88f2e07cd7b4ea93c334cfc18ce28
David Asher <david.asher@implisense.com>
2021-06-24 13:18:07 +0200

    write entry point for backend

--- backend/src/main/kotlin/get/me/a/job/Main.kt
+++ backend/src/main/kotlin/get/me/a/job/Main.kt
@@ -2,10 +2,22 @@ package get.me.a.job
 
 import get.me.a.job.api.siteApi
 import get.me.a.job.dao.MapDao
+import org.http4k.core.then
+import org.http4k.filter.CorsPolicy
+import org.http4k.filter.ServerFilters
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
-
+import kotlin.concurrent.thread
 
 fun main() {
-    siteApi(MapDao()).asServer(SunHttp(9000)).start()
+    val dao = MapDao()
+    thread {
+        while (true) {
+            checkTask(dao)
+            Thread.sleep(1500L)
+        }
+    }
+    ServerFilters.Cors(CorsPolicy.UnsafeGlobalPermissive)
+        .then(siteApi(dao))
+        .asServer(SunHttp(9000)).start()
 }
