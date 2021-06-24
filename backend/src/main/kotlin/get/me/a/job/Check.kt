edaee8720c23ed08227648175136e4b407d975e7
David Asher <david.asher@implisense.com>
2021-06-24 13:16:23 +0200

    dockerizte frontend and backend

--- backend/src/main/kotlin/get/me/a/job/Check.kt
+++ backend/src/main/kotlin/get/me/a/job/Check.kt
@@ -4,16 +4,32 @@ import get.me.a.job.dao.Dao
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging
 import java.net.URL
 
+private val LOGGER = KotlinLogging.logger { }
+
 fun checkTask(dao: Dao) {
+    LOGGER.info { "checking sites" }
     val sites = dao.getSites()
-
-    runBlocking(Dispatchers.IO) {
-       sites.map {
-           async {
-
-           }
-       }
+    val updated =
+        runBlocking {
+            sites
+                .map { site ->
+                    val newStatus = async(Dispatchers.IO) {
+                        try {
+                            LOGGER.info { site.url }
+                            URL(site.url).content
+                            true
+                        } catch (t: Throwable) {
+                            false
+                        }
+                    }
+                    site to newStatus
+                }
+                .map { (site, deferredStatus) -> site to deferredStatus.await() }
+        }
+    updated.forEach { (site, newStatus) ->
+        dao.updateState(site.id, newStatus)
     }
 }
